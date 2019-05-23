2b294ef4036794247c47391e4dff9fefddb0a2fd
Fine33e <damian.dluski@gmail.com>
2019-05-23 20:52:36 +0200

    work

--- app/src/main/java/com/projekt/damian/kalkulator/AdvancedCalculatorActivity.java
+++ app/src/main/java/com/projekt/damian/kalkulator/AdvancedCalculatorActivity.java
@@ -5,242 +5,157 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.mariuszgromada.math.mxparser.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class AdvancedCalculatorActivity extends AppCompatActivity {
 
-
-    Button btn7, btn8, btn9, btnDiv, btn4, btn5, btn6, btnMul
-            , btn1, btn2, btn3, btnSub, btn0, btnDot, btn00, btnSum,
-            btnC, btnChar, btnEqu, btnSqrt, btnLog, btnLn, btnSin, btnCos,
-            btnTan, btnPer, btnX2, btnXY;
     TextView tvInput, tvResult;
+
+    private Calculate calculate;
+    private Toast toast;
+    private final String TOAST_TEXT = "Nieprawidłowa operacja!";
+
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        ArrayList<String> arrayList = savedInstanceState.getStringArrayList("backup");
+        tvInput.setText(arrayList.get(0));
+        tvResult.setText(arrayList.get(1));
+        calculate.setStringBuilder(arrayList.get(1));
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putStringArrayList("backup", new ArrayList<String>(Arrays.asList(tvInput.getText().toString(),tvResult.getText().toString())));
+        super.onSaveInstanceState(outState);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.advanced_activity);
-
-        btn0 = findViewById(R.id.btn_0);
-        btn1 = findViewById(R.id.btn_1);
-        btn2 = findViewById(R.id.btn_2);
-        btn3 = findViewById(R.id.btn_3);
-        btn4 = findViewById(R.id.btn_4);
-        btn5 = findViewById(R.id.btn_5);
-        btn6 = findViewById(R.id.btn_6);
-        btn7 = findViewById(R.id.btn_7);
-        btn8 = findViewById(R.id.btn_8);
-        btn9 = findViewById(R.id.btn_9);
-        btn00 = findViewById(R.id.btn_00);
-        btnDiv = findViewById(R.id.btn_Div);
-        btnMul = findViewById(R.id.btn_Mul);
-        btnSub = findViewById(R.id.btn_Sub);
-        btnDot = findViewById(R.id.btn_Dot);
-        btnSum = findViewById(R.id.btn_Sum);
-        btnC = findViewById(R.id.btn_C);
-        btnChar = findViewById(R.id.btn_char);
-        btnEqu = findViewById(R.id.btn_Equ);
-        btnSqrt = findViewById(R.id.btn_sqrt);
-        btnLog = findViewById(R.id.btn_log);
-        btnLn = findViewById(R.id.btn_ln);
-        btnSin = findViewById(R.id.btn_sin);
-        btnCos = findViewById(R.id.btn_cos);
-        btnTan = findViewById(R.id.btn_tan);
-        btnPer = findViewById(R.id.btn_per);
-        btnX2 = findViewById(R.id.btn_x2);
-        btnXY = findViewById(R.id.btn_xy);
-
-        tvInput = findViewById(R.id.tv_input);
-        tvResult = findViewById(R.id.tv_result);
-
-        clearScreen();
-
-        btn0.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                inputExpression("0");
-            }
-        });
-        btn00.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                inputExpression("00");
-            }
-        });
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                inputExpression("1");
-            }
-        });
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                inputExpression("2");
-            }
-        });
-        btn3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                inputExpression("3");
-            }
-        });
-        btn4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                inputExpression("4");
-            }
-        });
-        btn5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                inputExpression("5");
-            }
-        });
-        btn6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                inputExpression("6");
-            }
-        });
-        btn7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                inputExpression("7");
-            }
-        });
-        btn8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                inputExpression("8");
-            }
-        });
-        btn9.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                inputExpression("9");
-            }
-        });
-        btnDiv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                inputExpression("/");
-            }
-        });
-        btnMul.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                inputExpression("*");
-            }
-        });
-        btnSub.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                inputExpression("-");
-            }
-        });
-        btnSum.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                inputExpression("+");
-            }
-        });
-        btnDot.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                inputExpression(".");
-            }
-        });
-        btnC.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                clearScreen();
-            }
-        });
-        btnChar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                inputExpression("+/-");
-            }
-        });
-        btnEqu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String expression = tvInput.getText().toString();
-
-                Expression e = new Expression(expression);
-                String result = String.valueOf(e.calculate());
-
-                tvResult.setText(result);
-            }
-        });
-        btnSqrt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                inputExpression("sqrt(");
-            }
-        });
-        btnLog.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                inputExpression("log(");
-            }
-        });
-        btnLn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                inputExpression("ln(");
-            }
-        });
-        btnSin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                inputExpression("sin(");
-            }
-        });
-        btnCos.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                inputExpression("cos(");
-            }
-        });
-        btnTan.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                inputExpression("tan(");
-            }
-        });
-        btnPer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                inputExpression("%");
-            }
-        });
-        btnX2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                inputExpression("^2");
-            }
-        });
-        btnXY.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                inputExpression("^");
-            }
-        });
+        tvInput = (TextView) findViewById(R.id.tv_input);
+        tvResult = (TextView) findViewById(R.id.tv_result);
+        toast = Toast.makeText(getApplicationContext(), TOAST_TEXT, Toast.LENGTH_LONG);
+        calculate = new Calculate(tvInput,tvResult,toast);
 
 
+    }
+    public void number(View v) {
+        switch (v.getId()) {
+            case R.id.btn_0:
+                calculate.makeNumber("0");
+                break;
+            case R.id.btn_00:
+                calculate.makeNumber("00");
+                break;
+            case R.id.btn_1:
+                calculate.makeNumber("1");
+                break;
+            case R.id.btn_2:
+                calculate.makeNumber("2");
+                break;
+            case R.id.btn_3:
+                calculate.makeNumber("3");
+                break;
+            case R.id.btn_4:
+                calculate.makeNumber("4");
+                break;
+            case R.id.btn_5:
+                calculate.makeNumber("5");
+                break;
+            case R.id.btn_6:
+                calculate.makeNumber("6");
+                break;
+            case R.id.btn_7:
+                calculate.makeNumber("7");
+                break;
+            case R.id.btn_8:
+                calculate.makeNumber("8");
+                break;
+            case R.id.btn_9:
+                calculate.makeNumber("9");
+                break;
 
+        }
+    }
+    public void simple(View view) {
+        switch (view.getId()) {
+            case R.id.btn_Sum:
+                calculate.clickOperator("+");
+                break;
+            case R.id.btn_Sub:
+                calculate.clickOperator("-");
+                break;
+            case R.id.btn_Mul:
+                calculate.clickOperator("*");
+                break;
+            case R.id.btn_Div:
+                calculate.clickOperator("/");
+                break;
+            case R.id.btn_char:
+                calculate.clickOperator("+/-");
+                break;
+            case R.id.btn_xy:
+                calculate.clickOperator("^");
+                break;
+        }
+    }
+    public void advanced(View view) {
+        switch (view.getId()) {
+            case R.id.btn_sin:
+                calculate.func("sin(");
+                break;
+            case R.id.btn_cos:
+                calculate.func("cos(");
+                break;
+            case R.id.btn_tan:
+                calculate.func("tan(");
+                break;
+            case R.id.btn_ln:
+                calculate.func("ln(");
+                break;
+            case R.id.btn_log:
+                calculate.func("log10(");
+                break;
+            case R.id.btn_sqrt:
+                calculate.func("sqrt(");
+                break;
+            case R.id.btn_x2:
+                calculate.func("^2");
+                break;
+        }
+    }
+    public void pow(View v){
+        calculate.pow();
+    }
+
+    public void percent(View v) {
+        calculate.percent();
+    };
+
+    public void delete(View v) {
+        calculate.delete();
+    }
+
+    public void sum(View v) {
+        calculate.sum();
+    }
+
+    public void plusMinus(View v) {
+        calculate.plusMinus();
     }
 
     void clearScreen() {
         tvInput.setText("");
         tvResult.setText("0");
     }
-    void inputExpression(String value){
-        String expression = tvInput.getText().toString();
-        expression = expression + value;
 
-        tvInput.setText(expression);
-    }
+
 }
--- app/src/main/java/com/projekt/damian/kalkulator/Calculate.java
+++ app/src/main/java/com/projekt/damian/kalkulator/Calculate.java
@@ -1,4 +1,272 @@
 package com.projekt.damian.kalkulator;
 
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.mariuszgromada.math.mxparser.Expression;
+
 public class Calculate {
+
+    private final String TOAST = "Wrong operation";
+
+    private StringBuilder stringBuilder;
+    private TextView screen;
+    private TextView secondScreen;
+    private Toast toast;
+    private int deleteCounter;
+    private Expression expression;
+    private boolean advanceFlag;
+    private boolean numberFlag;
+
+    public Calculate(TextView screen, TextView secondScreen, Toast toast) {
+        this.stringBuilder = new StringBuilder();
+        this.screen = screen;
+        this.secondScreen = secondScreen;
+        this.expression = new Expression();
+        this.toast = toast;
+        this.deleteCounter = 0;
+        this.advanceFlag = false;
+        this.numberFlag = false;
+    }
+    public void makeNumber(String number) {
+        if(isNounAtFirstPosition(number) && isnumberFirstPosition(number) && isMinus() && fullSize() && isPercent()) {
+            stringBuilder.append(number);
+            ifSecondNotComma(number);
+            numberFlag=true;
+        }
+        else {
+            toast.show();
+        }
+        secondScreen.setText(stringBuilder.toString());
+        deleteCounter = 0;
+    }
+
+    public void clickOperator(String operator) {
+        if(isGreaterThanZero() && isCommaAtEndString()) {
+            if (screen.getText() == null) {
+                screen.setText(stringBuilder.append(operator).toString());
+            } else {
+                if(advanceFlag){
+                    advanceFlag=false;
+                    stringBuilder.append(')');
+                }
+                screen.setText(screen.getText() + stringBuilder.append(operator).toString());
+            }
+
+            delete();
+            deleteCounter = 0;
+        }
+        else if(screen.getText().length()>0) {
+            changeOperator(operator);
+        }
+        else {
+            toast.show();
+        }
+    }
+    private boolean isGreaterThanZero() {
+        return stringBuilder.length() > 0;
+    }
+
+    private boolean isGreaterThanOne() {
+        return stringBuilder.length() > 1;
+    }
+
+    private boolean fullSize(){
+        return stringBuilder.length()<15;
+    }
+
+    private boolean isCommaAtEndString(){
+        return !(stringBuilder.charAt(stringBuilder.length()-1)=='.');
+    }
+
+
+
+    public void delete() {
+        advanceFlag=false;
+        numberFlag=false;
+        deleteCounter++;
+        if(deleteCounter == 2){
+            deleteAll();
+        }
+        stringBuilder = new StringBuilder();
+        secondScreen.setText(null);
+    }
+
+    public void deleteAll() {
+        advanceFlag=false;
+        numberFlag=false;
+        deleteCounter = 0;
+        stringBuilder = new StringBuilder();
+        screen.setText(null);
+        secondScreen.setText(null);
+    }
+
+    private void ifSecondNotComma(String input){
+        if(isGreaterThanOne()){
+            if((stringBuilder.charAt(0)=='0' && !input.contains(".") && stringBuilder.charAt(1)!='.')){
+                stringBuilder.deleteCharAt(0);
+            }
+        }
+    }
+
+    private boolean isMinus(){
+        return !stringBuilder.toString().contains("-");
+    }
+
+    private void changeOperator(String input){
+        StringBuilder topScreenValue = new StringBuilder(screen.getText());
+        if(topScreenValue.charAt(1) != input.charAt(0)){
+            topScreenValue.setCharAt(topScreenValue.length()-1,input.charAt(0));
+            screen.setText(topScreenValue);
+        }
+    }
+
+    private boolean isNounAtFirstPosition(String input){
+        if(isGreaterThanZero()) {
+            return !(stringBuilder.charAt(0) == '0' && !stringBuilder.toString().contains(".") && input == "0");
+        }
+        return true;
+    }
+
+    private boolean isnumberFirstPosition(String input){
+        if(input.contains(".") && !stringBuilder.toString().contains(".") && isGreaterThanZero()) {
+            return true;
+        }
+        else if(!input.contains(".")){
+            return true;
+        }
+        return false;
+    }
+
+    private String deleteFraction(String input) {
+        if (input.charAt(input.length() - 2) == '.' && input.charAt(input.length() - 1) == '0') {
+            return input.substring(0, input.length() - 2);
+        }
+        return input;
+    }
+
+
+    private boolean divisionByZero(){
+        String mainScreenText = screen.getText().toString();
+        return mainScreenText.contains("/0") || ((mainScreenText.charAt(mainScreenText.length()-1) == '/') && !stringBuilder.toString().contains(".") && stringBuilder.charAt(0) == '0');
+    }
+
+    private boolean isJustZero(){
+        if(isGreaterThanZero()){
+            if((stringBuilder.length()==1 && stringBuilder.charAt(0)=='0'))
+                return false;
+        }
+        return true;
+    }
+
+    private boolean isGreaterThanZeroScreen(){
+        return screen.getText().length()>0 && isGreaterThanZero();
+    }
+
+
+
+    public void sum() {
+        if(isGreaterThanZeroScreen() && !divisionByZero() && isCommaAtEndString()) {
+            if(advanceFlag){
+                advanceFlag=false;
+                stringBuilder.append(')');
+            }
+            String value = screen.getText() + stringBuilder.toString();
+            expression.setExpressionString(value);
+            stringBuilder = new StringBuilder(deleteFraction(String.valueOf(expression.calculate())));
+            secondScreen.setText(stringBuilder.toString());
+            screen.setText(null);
+            numberFlag=false;
+        }
+        else if(advanceFlag){
+            if((stringBuilder.toString().contains("ln") || stringBuilder.toString().contains("ln")) && stringBuilder.charAt(stringBuilder.length()-1)=='0' && stringBuilder.charAt(stringBuilder.length()-2) =='('){
+                toast.show();
+            }
+            else {
+                advanceFlag = false;
+                stringBuilder.append(')');
+                expression.setExpressionString(stringBuilder.toString());
+                stringBuilder = new StringBuilder(deleteFraction(String.valueOf(expression.calculate())));
+                secondScreen.setText(stringBuilder.toString());
+            }
+        }
+        else {
+            toast.show();
+        }
+
+    }
+
+    private boolean isPercent(){
+        if(isGreaterThanZero()){
+            return !stringBuilder.toString().contains("%");
+        }
+        return true;
+    }
+    public void pow() {
+        if(isGreaterThanZero() && !stringBuilder.toString().contains("^")){
+            stringBuilder.append('^');
+            screen.setText(stringBuilder);
+            stringBuilder = new StringBuilder("2");
+            secondScreen.setText(stringBuilder);
+        }
+        else {
+            toast.show();
+        }
+    }
+
+    public void plusMinus() {
+        if(isGreaterThanZero() && isCommaAtEndString() && isJustZero() && !advanceFlag) {
+            if (stringBuilder.toString().contains("-")) {
+                stringBuilder.delete(0, 2);
+                stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+            } else {
+                stringBuilder.insert(0, '(');
+                stringBuilder.insert(1, '-');
+                stringBuilder.insert(stringBuilder.length(), ')');
+            }
+            secondScreen.setText(stringBuilder.toString());
+        }
+        else if(advanceFlag && !stringBuilder.toString().contains("ln") && !stringBuilder.toString().contains("log") && !stringBuilder.toString().contains("sqrt")){
+            if(!stringBuilder.toString().contains("-")) {
+                stringBuilder.insert(4, '-');
+            }
+            else{
+                stringBuilder.deleteCharAt(4);
+            }
+
+            secondScreen.setText(stringBuilder.toString());
+
+        }
+        else {
+            toast.setText(TOAST);
+            toast.show();
+        }
+    }
+
+    public void setStringBuilder(String stringBuilder) {
+        this.stringBuilder = new StringBuilder(stringBuilder);
+    }
+
+    public void percent() {
+        if(isPercent() && isGreaterThanZeroScreen()){
+            stringBuilder.append('%');
+            secondScreen.setText(stringBuilder);
+        }
+        else {
+            toast.show();
+        }
+    }
+
+    public void func(String input){
+        if(!numberFlag && !advanceFlag) {
+            advanceFlag = true;
+            stringBuilder.append(input);
+            secondScreen.setText(stringBuilder);
+        }
+        else {
+            toast.show();
+        }
+    }
+
+
 }
